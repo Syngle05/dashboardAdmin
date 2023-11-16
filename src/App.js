94d18d6d1f50e65f8a6c0d62ddef089350a0f405
Heitor <torbeckpires@gmail.com>
2023-11-15 21:01:31 -0300

    HasRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthProvider from "./context/AuthProvider/AuthProvider";
